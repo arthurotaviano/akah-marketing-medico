8ac59916f2bdab6047f9749d3fc33807626bd902
Arthur Otaviano <oarthurotaviano@gmail.com>
2026-08-24 19:42:47 -0300

    refactor: replace interface for type alias and adjust font size

--- src/components/link-button.tsx
+++ src/components/link-button.tsx
@@ -1,9 +1,9 @@
 import { ArrowUpRight } from 'lucide-react'
 
-interface LinkButtonProps {
+type LinkButtonProps = {
   variant?: 'gradient' | 'solid'
   href: string
-  target?: '_blank' | '_self' | '_parent' | '_top'
+  target?: '_blank'
   children: React.ReactNode
 }
 
@@ -16,7 +16,7 @@ const variants = {
 export function LinkButton({ variant = 'gradient', href, target, children }: LinkButtonProps) {
   return (
     <a
-      className={`inline-flex items-center gap-1 whitespace-nowrap rounded-full px-5 py-2 font-medium ${variants[variant]}`}
+      className={`inline-flex items-center gap-1 whitespace-nowrap rounded-full px-5 py-2 text-sm md:text-base font-medium ${variants[variant]}`}
       href={href}
       target={target}
     >
