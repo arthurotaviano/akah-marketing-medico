18f275a2b5928855d5618599d1ade763b4c74ca9
Arthur Otaviano <oarthurotaviano@gmail.com>
2026-09-02 14:22:22 -0300

    refactor(footer): move links info to a separated file inside data folder

--- src/components/footer/links.tsx
+++ src/components/footer/links.tsx
@@ -1,27 +1,4 @@
-import { CONTACT_INFO, CONTACT_LINKS } from '@/constants/contact'
-import { SiInstagram, SiWhatsapp } from '@icons-pack/react-simple-icons'
-
-type LinkProps = {
-  name: string
-  href: string
-  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>
-  label: string
-}
-
-const links: LinkProps[] = [
-  {
-    name: 'Instagram',
-    icon: SiInstagram,
-    href: CONTACT_LINKS.INSTAGRAM,
-    label: `@${CONTACT_INFO.INSTAGRAM_USER}`,
-  },
-  {
-    name: 'WhatsApp',
-    href: CONTACT_LINKS.WHATSAPP,
-    icon: SiWhatsapp,
-    label: CONTACT_INFO.WHATSAPP,
-  },
-]
+import { links } from '@/data/footer'
 
 export function Links() {
   return (
